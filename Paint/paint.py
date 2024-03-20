489776997ebc75a7a3fc0a5c9b5539d9a40e7491
AlejandroGarcia03 <a01641920@tec.mx>
2024-03-20 09:30:04 -0600

    Update paint.py

--- Paint/paint.py
+++ Paint/paint.py
@@ -35,32 +35,12 @@ def circle2(start, end):
 # Función para dibujar un rectángulo desde el punto de inicio hasta el punto final
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
 
-    for _ in range(2):  # Repetir dos veces para completar el rectángulo
-        forward(end.x - start.x)
-        left(90)
-        forward(end.y - start.y)
-        left(90)
-
-    end_fill()
 
 # Función para dibujar un triángulo desde el punto de inicio hasta el punto final
 def triangle(start, end):
     """Draw triangle from start to end."""
-    up()
-    goto(start.x, start.y)
-    down()
-    begin_fill()
 
-    for _ in range(3):  # Repetir tres veces para completar el triángulo
-        forward(end.x - start.x)
-        left(120)
-
-    end_fill()
 
 def tap(x, y):
     """Store starting point or draw shape."""
@@ -82,13 +62,13 @@ def info_alumnos():
     t.up()
     t.goto(0,190)
     t.color('blue')
-    t.write('Cristian Alejandro Garcia Mendoza', align='left', font=('Arial', 10, 'normal'))
+    t.write('Cristian Alejandro Garcia Mendoza A01641920', align='left', font=('Arial', 10, 'normal'))
     t.goto(0,170)
     t.color('pink')
-    t.write('Cesar Alejandro Benavides ', align='left', font=('Arial', 10, 'normal'))
+    t.write('Cesar Alejandro Benavides A01285056', align='left', font=('Arial', 10, 'normal'))
     t.goto(0,150)
     t.color('green')
-    t.write('Rodrigo Ibarra ', align='left', font=('Arial', 10, 'normal'))
+    t.write('Rodrigo Ibarra A01625569', align='left', font=('Arial', 10, 'normal'))
 
 # Inicializar el estado y configurar la ventana
 #Variable sate es un Diccionario
